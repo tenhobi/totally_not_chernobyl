0a1467c0916ce0d5b9a218aab11fe6f003b65ee2
Honza Bittner <git@tenhobi.dev>
2020-01-14 15:28:30 +0100

    Fix

--- tool/ci.sh
+++ tool/ci.sh
@@ -5,6 +5,5 @@ set -o nounset # exit script when it tries to use undeclared variables
 set -o xtrace # traces commands before executing them
 
 flutter format --set-exit-if-changed .
-flutter packages get
 flutter analyze --no-congratulate --no-pub
 flutter test --coverage --coverage-path coverage/lcov.info
