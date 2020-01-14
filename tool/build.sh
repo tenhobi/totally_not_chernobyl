73869dbc794affd3777bf575f09a15c0cb4772e3
Honza Bittner <git@tenhobi.dev>
2020-01-14 18:08:29 +0100

    Fix A1

--- tool/build.sh
+++ tool/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # inputs: Flutter in PATH
-# outputs: coverage in coverage/lcov.info
+# outputs: build for $1 in build/
 
 set -o errexit # exit script when command fails
 set -o nounset # exit script when it tries to use undeclared variables
 set -o xtrace # traces commands before executing them
 
-flutter format --set-exit-if-changed .
-flutter pub get
-flutter analyze --no-congratulate --no-pub
-flutter test --coverage --coverage-path coverage/lcov.info
+flutter build $1
